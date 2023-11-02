a6c482586f8cd2dc6e17c30e165c97840daad131
iAmSomething <st939823@gmail.com>
2023-11-02 09:02:14 +0900

    Project: 프로젝트 생성

---
+++ ReadProduct/ReadProduct/ReadProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadProductApp.swift
+//  ReadProduct
+//
+//  Created by 김태훈 on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ReadProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
