446eddab228f65d442acdfec2b4f8d98e30983f6
iAmSomething <st939823@gmail.com>
2023-11-06 15:07:10 +0900

    Config: 초기 셋팅 1. Color custom 등록 2. custom Font 등록 3. ImagePicker 추가

--- ReadProduct/ReadProduct/ContentView.swift
+++ ReadProduct/ReadProduct/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Text("Hello, world!").font(.appFont(for: .Thin, size: 25))
         }
         .padding()
     }
---
+++ ReadProduct/ReadProduct/Resource/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  ReadProduct
+//
+//  Created by 김태훈 on 11/6/23.
+//
+
+import Foundation
+import SwiftUI
+extension Color {
+  static let appRed: Color = Color(red: 1, green: 0.43, blue: 0.38)
+  static let appYellow: Color = Color(red: 0.97, green: 0.82, blue: 0.38)
+  static let appYellowPale: Color = Color(red: 0.95, green: 0.91, blue: 0.62)
+  static let appGreen: Color = Color(red: 0.6, green: 0.85, blue: 0.67)
+  static let appTextLightGray: Color = Color(red: 0.85, green: 0.85, blue: 0.85)
+  static let appTextDarkGray: Color = Color(red: 0.59, green: 0.59, blue: 0.59)
+}
