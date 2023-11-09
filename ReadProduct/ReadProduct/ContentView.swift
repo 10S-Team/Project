0510ad81ac5d3a908fd5179c765f2df0e9fe3fbd
김태훈 <38745420+iAmSomething@users.noreply.github.com>
2023-11-09 18:59:00 +0900

    Refactoring#7 앱 이름 반영 + 기타 추가사항 (#9)
    * Color 구체화와 런치스크린
    트러블슈팅
     - info.plist에서 launch screen 항목을 수정하는 방식이 있다고 해서 시도해 봤는데 안됐다.
     -> 그래서 그냥 storyboard파일로 했다
     - Dark 모드를 반영할 수 있는 함수를 Color에 추가했다.
     -> colorSceme Environment를 매번 써주어야 하는데 이 부분 궁리해야할듯
    
    * 접근성 추가 해봄
    
    - 보이스오버로 안 읽어지는 이슈가 있음
    
    * 보이스오버 읽어주는 기능 트러블슈팅
     - 한글 string은 안 읽히고 영어만 읽히는 상황
     - 해결: xcode의 Localization을 korean으로 하고 Default로 설정

--- ReadProduct/ReadProduct/ContentView.swift
+++ ReadProduct/ReadProduct/ContentView.swift
@@ -6,16 +6,56 @@
 //
 
 import SwiftUI
-
+import PhotosUI
 struct ContentView: View {
+    @Environment(\.colorScheme) var scheme
+    @State private var image: UIImage?
+    @State private var sourceType: UIImagePickerController.SourceType = .camera
+    @State private var callPicker: Bool = false
     var body: some View {
         VStack {
-          Image(.camera)
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-          Text("Hello, world!").font(.appFont(for: .Thin, size: 25))
-        }
-        .padding()
+            Text("상품을 찍어서 읽어보아요!")
+            .font(.appFont(for: .Thin, size: 30))
+            .foregroundColor(.appColor(type: .appTextBlack,scheme: scheme))
+            .padding(.top,50)
+            .accessibilityLabel("상품을 찍어서 읽어보아요!")
+            
+            ZStack {
+                Rectangle()
+                    .foregroundStyle(Color.clear)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.appColor(type: .appYellow,scheme: scheme))
+                    .cornerRadius(20)
+                Image(.camera)
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    .accessibilityHidden(true)
+            }.padding(.horizontal,30)
+                .padding(.top,50)
+                .padding(.bottom,100)
+                .onTapGesture {
+                    sourceType = .camera
+                    callPicker = true
+                }
+                .accessibilityLabel(.init("사진을 찍어요"))
+            Button(action: {
+                sourceType = .photoLibrary
+                callPicker = true
+            },
+                   label: {
+                Text("사진에서 불러오기")
+                    .font(.appFont(for: .ExtraBold, size: 20))
+                    .foregroundStyle(Color.appColor(type: .appRed,scheme: scheme))
+            }).foregroundColor(.clear)
+                .frame(maxWidth: .infinity, maxHeight: 56)
+                .background(Color.appColor(type: .appYellow,scheme: scheme))
+                .cornerRadius(20)
+                .accessibilityLabel("사진을 불러와요")
+        }.fullScreenCover(isPresented: $callPicker,
+                          content: {
+            ImagePicker(image: $image, type: sourceType)
+                .ignoresSafeArea()
+        }).padding()
     }
 }
 
