12ee4cf87ec96e3f1c9819687ca8010c69be2870
sooobee <20subi@jbnu.ac.kr>
2023-11-10 12:13:40 +0900

    사이즈 재설정, 갤러리 버튼 색 변경

--- ReadProduct/ReadProduct/ContentView.swift
+++ ReadProduct/ReadProduct/ContentView.swift
@@ -13,10 +13,9 @@ struct ContentView: View {
             
             Text("상품을 찍어서 읽어보아요!")
                 .font(.appFont(for: .Bold, size: 25))
-                .frame(maxWidth: .infinity, alignment: .center)
+                .frame(width: .infinity, height: .infinity, alignment: .center)
                 .padding(.top, 70)
-    
-            Spacer()
+                //.padding()
             
             // 사진찍기 버튼
             Button(action: {
@@ -25,33 +24,47 @@ struct ContentView: View {
                 
                 VStack{
                     Image(.camera)
+                        .aspectRatio(contentMode: .fill)
                         .imageScale(.large)
-                        .frame(maxWidth: .infinity, maxHeight: 400, alignment: .center)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
                         
                     Text("카메라")
                         .foregroundStyle(Color.black)
-                        .font(.appFont(for: .Thin, size: 20))
+                        .font(.appFont(for: .Regular, size: 20))
+                        //.multilineTextAlignment(.center)
+                        .padding()
+                        
                 }
             })
-            
+            .padding(.horizontal, 50)
+            .padding(.top, 30)
             .buttonStyle(.borderedProminent)
             .tint(Color.appYellow)
+            .frame(alignment: .center)
             
-            Spacer()
+            
+            //Spacer()
             
             // 사진 불러오기 버튼
             Button(action: {
                 // 코드 작성
             }, label: {
-                Text("사진 불러오기")
+                Text("사진에서 불러오기")
                     .foregroundStyle(Color.black)
                     .font(.appFont(for: .Regular, size: 20))
-                    .frame(maxWidth: .infinity, maxHeight: 100)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    //.frame(width: .infinity, height: .infinity)
+                    
             })
             .buttonStyle(.borderedProminent)
-            .tint(Color.appTextLightGray)
-        }
-        .padding()
+            .tint(Color.appYellow)
+            .padding(.top, 130)
+            //.aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+
+        } // VStack
+        .edgesIgnoringSafeArea(.bottom)
+        //.padding()
     }
 }
 
