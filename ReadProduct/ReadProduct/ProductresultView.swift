6947a0e105e04d8b9217be02de4353c66d573fb4
wonzzang1 <bbenpoolkim@naver.com>
2023-11-26 14:42:38 +0900

    이미지화면을 구성하여 상품검색뷰를 만들었습니다.

---
+++ ReadProduct/ReadProduct/ProductresultView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductresultView.swift
+//  ReadProduct
+//
+//  Created by 김회원 on 11/26/23.
+//
+
+import SwiftUI
+
+struct ProductresultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductresultView()
+}
